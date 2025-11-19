42bd5a20de7293146de9b29687b94222ad666245
xscriptor <preciado.oscar.osorio@gmail.com>
2025-11-19 20:32:53 +0100

    update keyring hook

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-# Inicializa y prepara el keyring de pacman dentro del airootfs del ISO.
-# Sin TTY checks, sin autostart, sin countdown.
 set -euo pipefail
 
 pacman-key --init
 pacman-key --populate archlinux
-pacman -Sy --noconfirm archlinux-keyring
